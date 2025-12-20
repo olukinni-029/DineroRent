e5fad7f660f07597ccb41d8225dd708135e0264a
OLUKINNI <109589367+olukinni-029@users.noreply.github.com>
2025-12-20 18:45:12 +0100

    Update payment.ts
    wip

--- src/utils/payment.ts
+++ src/utils/payment.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 
-const PAYSTACK_BASE_URL = 'https://api.paystack.co';
+const PAYSTACK_BASE_URL = process.env.PAYSTACK_BASE_URL || 'https://api.paystack.co';
 const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
 
 export interface PaystackPaymentData {
