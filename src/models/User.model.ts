5ffe4663d0abd015782e72dfe5348b00ddcebb0b
OLUKINNI <109589367+olukinni-029@users.noreply.github.com>
2026-01-21 18:53:21 +0100

    Update User.model.ts
    wiq

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,28 +1,30 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  password: string;
-  role: 'user' | 'super_admin' | 'vendor_verification_admin' | 'finance_admin' | 'support_admin';
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+  password?: string;
+  role?: 'user' | 'super_admin' | 'vendor_verification_admin' | 'finance_admin' | 'support_admin';
   avatar?: string;
   username?: string;
-  phoneNumber?: string;
   deactivated?: boolean;
   suspended?: boolean;
 }
 
 const UserSchema = new Schema<IUser>(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    firstName: { type: String},
+    lastName: { type: String },
+    email: { type: String, unique: true },
+    phone: { type: String, unique: true },
+    password: { type: String,},
     role: { type: String, enum: ['user', 'super_admin', 'vendor_verification_admin', 'finance_admin', 'support_admin'], default: 'user' },
     avatar: { type: String },
+    username: { type: String, unique: true },
+    deactivated: { type: Boolean, default: false },
+    suspended: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
